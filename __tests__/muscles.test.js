ab3b9e4f8e9589101999a4fef5a9f68b2001be71
Victor Caruso <vic.caruso@gmail.com>
2022-03-18 12:16:29 -0700

    set up MuscleCars model, controller, and first failing test

---
+++ __tests__/muscles.test.js
@@ -0,0 +1,27 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const MuscleCar = require('../lib/models/MuscleCar');
+
+describe('hand-of-resources routes for muscle cars', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('Creates a muscle car in the muscle_cars db', async () => {
+    const expected = {
+      id: expect.any(String),
+      make: 'Chevrolet',
+      model: 'Corvette L88',
+      year: 1968
+    };
+    const res = await request(app).post('/api/v1/musclecars').send(expected);
+    expect(res.body).toEqual(expected);
+  });
+
+});
