871edc08977291987b159903c8895c31887f6254
Victor Caruso <vic.caruso@gmail.com>
2022-03-18 14:14:29 -0700

    set up test to get single  book by id

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -26,7 +26,13 @@ describe('hand-of-resources routes for muscle cars', () => {
 
   it('Gets all books from table', async () => {
     const expected = await Book.getAll();
-    const res = await request(app).get('/api/v1/books').send(expected);
+    const res = await request(app).get('/api/v1/books');
+    expect(res.body).toEqual(expected);
+  });
+
+  it('Gets a single book by id from table', async () => {
+    const expected = await Book.getById(1);
+    const res = await request(app).get('/api/v1/books/1');
     expect(res.body).toEqual(expected);
   });
 });
