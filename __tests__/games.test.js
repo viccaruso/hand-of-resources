5002c609714a9c692c81e426b9b332beee23cdb8
Victor Caruso <vic.caruso@gmail.com>
2022-03-18 16:35:40 -0700

    created insert test for games table

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Game = require('../lib/models/Book');
+const { use } = require('../lib/controllers/victors');
 
 describe('hand-of-resources routes for muscle cars', () => {
   beforeEach(() => {
@@ -13,5 +14,18 @@ describe('hand-of-resources routes for muscle cars', () => {
     pool.end();
   });
 
+  it('Inserts a new game row into the database', async () => {
+    const expected = {
+      id: expect.any(String),
+      title: 'Horizon: Zero Dawn',
+      platform: 'PC',
+      genre: 'Action RPG',
+      has_played: false
+    };
+    const res = await request(app).post('/api/v1/games').send(expected);
+    expect(res.body).toEqual(expected);
+  }
   
+  );
 });
+
